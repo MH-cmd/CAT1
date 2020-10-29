41b704dee4c71493a2afd350ebaaccb1134ee807
hp <hp@DESKTOP-3V165GQ>
2020-10-29 21:34:22 +0300

    Facebook

---
+++ src/facebook/gridpane.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facebook;
+
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Mohammed Hiribae <BBIT@22>
+ */
+class gridpane {
+
+    static void add(TextField textField1, int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
